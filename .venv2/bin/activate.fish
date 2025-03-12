2413575bf7d28adf1d25ce956196bc628f2bff40
jortiz <jortiz.olivares@gmail.com>
2025-03-12 12:25:56 -0300

    Update changes

--- .venv2/bin/activate.fish
+++ .venv2/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ortijm/Documents/Proyecto-Django/3hmining-2/.venv2"
+set -gx VIRTUAL_ENV "/Users/ortijm/Documents/Proyecto-Django/3hmining-2-updated/.venv2"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
